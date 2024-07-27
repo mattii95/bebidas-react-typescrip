941cf82e4de738eb39b9e1f4aadd8ea466772ce3
Matias Courroux <mcourroux@hotmail.com>
2024-07-27 18:30:10 -0300

    mejoras de perfomance con React Router DOM

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,9 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import IndexView from "./views/IndexView"
-import FavoritesView from "./views/FavoritesView"
 import Layout from "./layouts/Layout"
+
+const FavoritesView = lazy(() => import('./views/FavoritesView'))
 
 export default function AppRouter() {
     return (
@@ -9,7 +11,11 @@ export default function AppRouter() {
             <Routes>
                 <Route element={<Layout />}>
                     <Route path="/" element={<IndexView />} index />
-                    <Route path="/favorites" element={<FavoritesView />} />
+                    <Route path="/favorites" element={
+                        <Suspense fallback="Cargando...">
+                            <FavoritesView />
+                        </Suspense>
+                    } />
                 </Route>
             </Routes>
         </BrowserRouter>
